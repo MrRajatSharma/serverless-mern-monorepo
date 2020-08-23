964bf0decb97f7c9b0ce7b171185a0be50eacd60
Rajat Sharma <er.rajat15@gmail.com>
2020-08-24 00:45:43 +0530

    express example

---
+++ packages/tweetsService/src/express.js
@@ -0,0 +1,27 @@
+// example using express
+
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+ 
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+ 
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+ 
+const server = new ApolloServer({ typeDefs, resolvers });
+ 
+const app = express();
+server.applyMiddleware({ app });
+ 
+app.listen({ port: 4001 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`)
+);
