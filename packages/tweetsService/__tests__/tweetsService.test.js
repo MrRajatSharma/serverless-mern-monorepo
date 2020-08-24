e381b7c8606678715bea4f8560326a208bb24d86
Rajat Sharma <er.rajat15@gmail.com>
2020-08-24 07:42:44 +0530

    fixes

--- packages/tweetsService/__tests__/tweetsService.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const tweetsService = require('..');
-
-describe('tweetsService', () => {
-    it('needs tests');
-});
--- packages/tweetsService/src/index.js
+++ packages/tweetsService/src/index.js
@@ -1,8 +1,49 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server-lambda");
+// const { ApolloServer, gql } = require("apollo-server");
 const { buildFederatedSchema } = require("@apollo/federation");
 
-const typeDefs = require("./typeDefs");
-const resolvers = require("./resolvers");
+const tweets = [
+  {
+    id: "1",
+    user: "Ada Lovelace",
+    title: "Just another tweet",
+    created_at: ""
+  },
+  {
+    id: "2",
+    user: "Alan Turing",
+    title: "What is this happening",
+    created_at: ""
+  }
+];
+
+const resolvers = {
+  Query: {
+    first() {
+      return tweets[0];
+    },
+    listAll() {
+      return tweets;
+    }
+  },
+  Tweet: {
+    __resolveReference(object) {
+      return tweets.find(tweet => tweet.id === object.id);
+    }
+  }
+};
+
+const typeDefs = gql`
+extend type Query {
+  first: Tweet,
+  listAll: [Tweet!]!
+}
+type Tweet @key(fields: "id") {
+  id: ID!
+  user: String
+  title: String
+  created_at: String
+}`;
 
 const server = new ApolloServer({
   schema: buildFederatedSchema([
@@ -13,13 +54,18 @@ const server = new ApolloServer({
   ])
 });
 
-// uncomment below to run as node env instead of serverless
-server.listen({ port: 4001 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+// export const handler = server.createHandler();
 
-// exports.handler = server.createHandler({
-//   cors: {
-//     origin: '*',
-//   },
+// uncomment below to run as node env instead of serverless
+// server.listen({ port: 4001 }).then(({ url }) => {
+//   console.log(`🚀 Server ready at ${url}`);
 // });
+
+export const handler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+    methods: 'GET, POST',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  },
+});
