882efe99d96b713f7a6f5c412429bd5fe4d5e96f
Rajat Sharma <er.rajat15@gmail.com>
2020-08-28 02:42:11 +0530

    loaded first page of tweets from Dynamodb using Graphql

--- packages/frontend/src/components/HomePage.js
+++ packages/frontend/src/components/HomePage.js
@@ -1,38 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Grid from "./Grid";
+import { useQuery, gql } from '@apollo/client';
 
-import tweets from './dataset';
 
-class HomePage extends React.Component {
-  
-  render() {
-    const items = tweets.map((tweet, index) => {
-      return (
-        <li
-          key={`tweet-${tweet.id}`}
-          className="grid-item"
-          style={{
-            width: 200,
-            // marginBottom: 20
-          }}
-        >
-          <div>
-            <h3>{tweet.user}</h3>
-            <em>{tweet.created_at}</em>
-            <p>{tweet.title}</p>
-          </div>
-        </li>
-      );
-    });
-
-    return (
-      <div>
-        <Grid >
-          {items}
-        </Grid>
-      </div>
-    );
+export const LIST_QUERY = gql`
+  query tweets {
+    list {
+      count,
+      lastEvaluatedKey,
+      scannedCount,
+      list {
+        id,
+        user,
+        title,
+        createdAt
+      }
+  	}
   }
+`
+
+export default () => {
+  const { loading, error, data } = useQuery(LIST_QUERY);
+  const [items, setItems] = useState([]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  useEffect(() => {
+    console.log("data", data);
+    if (data) {
+      const items = data && data.list.list.map((tweet, index) => {
+        return (
+          <li
+            key={`tweet-${tweet.id}`}
+            className="grid-item"
+            style={{
+              width: 200,
+              // marginBottom: 20
+            }}
+          >
+            <div>
+              <h3>{tweet.id}</h3>
+              <h3>{tweet.user}</h3>
+              {/* <em>{tweet.createdAt}</em> */}
+              <p>{tweet.title}</p>
+            </div>
+          </li>
+        );
+      });
+      setItems(items);
+    }
+  }, [data]);
+
+  const getQueryParams = () => {
+    return {  }
+  }
+
+  return (
+    <Grid >
+      {items}
+    </Grid>
+  );
 };
 
-export default HomePage;
