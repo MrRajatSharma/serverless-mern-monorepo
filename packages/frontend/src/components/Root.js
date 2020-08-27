c1c736f55c8e411d7be096a3d84b2f6868881425
Rajat Sharma <er.rajat15@gmail.com>
2020-08-28 02:42:18 +0530

    config

--- packages/frontend/src/components/Root.js
+++ packages/frontend/src/components/Root.js
@@ -4,13 +4,22 @@ import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
 import App from './App';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4001',
+  cache: new InMemoryCache()
+});
+
 export default class Root extends Component {
   render() {
     const { store, history } = this.props;
     return (
       <Provider store={store}>
         <ConnectedRouter history={history}>
-          <App />
+          <ApolloProvider client={client}>
+            <App />
+          </ApolloProvider>
         </ConnectedRouter>
       </Provider>
     );
