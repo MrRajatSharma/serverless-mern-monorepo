b5a1f2ed9fed86c6db1fff8c10da2314713f8570
Rajat Sharma <er.rajat15@gmail.com>
2020-08-30 01:10:20 +0530

    rendering new tweet

--- packages/frontend/src/components/HomePage.js
+++ packages/frontend/src/components/HomePage.js
@@ -3,6 +3,7 @@ import PinterestGrid from "./Grid";
 import { useQuery, gql } from '@apollo/client';
 
 import BlogPostAdd from "./BlogPostAdd";
+import { cloneDeep } from '@apollo/client/utilities';
 
 export const LIST_QUERY = gql`
   query tweets {
@@ -24,27 +25,33 @@ export default () => {
   const { loading, error, data } = useQuery(LIST_QUERY);
   const [items, setItems] = useState([]);
 
+  const renderItem = (tweet) => {
+    return <li
+    key={`tweet-${tweet.id}`}
+    className="grid-item"
+    style={{
+      width: 200,
+      // marginBottom: 20
+    }}
+  >
+    <div>
+      <h3>{tweet.id}</h3>
+      <h3>{tweet.user}</h3>
+      {/* <em>{tweet.createdAt}</em> */}
+      <p>{tweet.title}</p>
+    </div>
+  </li>
+  }
+
+  const updateItems = (list) => {
+    const newItems = list.map((tweet, index) => renderItem(tweet));
+    setItems(newItems);
+  }
+
   useEffect(() => {
     console.log("data", data);
     if (data) {
-      const newItems = data.list.list.map((tweet, index) => (
-        <li
-          key={`tweet-${tweet.id}`}
-          className="grid-item"
-          style={{
-            width: 200,
-            // marginBottom: 20
-          }}
-        >
-          <div>
-            <h3>{tweet.id}</h3>
-            <h3>{tweet.user}</h3>
-            {/* <em>{tweet.createdAt}</em> */}
-            <p>{tweet.title}</p>
-          </div>
-        </li>
-      ));
-      setItems(newItems);
+      updateItems(data.list.list);
     }
   }, [data]);
 
@@ -59,7 +66,14 @@ export default () => {
   return (
     <>
       <h1 style={{ color: "white"}}>Pinterest</h1>
-      <BlogPostAdd />
+      <BlogPostAdd
+        appendTweet={(newTweet) => {
+          const cloned = cloneDeep(items);
+          const renderedTweet = renderItem(newTweet.createTweet);
+          cloned.unshift(renderedTweet);
+          setItems(cloned);
+        }}
+      />
       <PinterestGrid >
         {items}
       </PinterestGrid>
