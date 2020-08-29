4961316276660a0b4656c79e5afffda5a752fc24
Rajat Sharma <er.rajat15@gmail.com>
2020-08-30 00:26:52 +0530

    add tweet mutation

--- packages/frontend/src/components/BlogPostAdd.js
+++ packages/frontend/src/components/BlogPostAdd.js
@@ -1,30 +1,78 @@
 import React, { useEffect, useState } from 'react';
-import PinterestGrid from "./Grid";
-import { useQuery, gql } from '@apollo/client';
+import { useMutation, gql } from '@apollo/client';
 
 import Button from '@material-ui/core/Button';
-import Fab from '@material-ui/core/Fab';
 import Grid from '@material-ui/core/Grid';
 
+export const CREATE_TWEET = gql`
+mutation CreateTweetMutation (
+  $title: String!
+  $user: String!
+  $body: String!
+) {
+  createTweet (
+    title: $title
+    user: $user
+    body: $body
+  ) {
+    id,
+    user,
+    title,
+    createdAt
+  } 
+}`
 
 export default () => {
+  let title, user, body;
+  const [addTweet, { data }] = useMutation(CREATE_TWEET);
+
   return (
-    <div className="pinterest-form">
+    <form
+      className="pinterest-form"
+      onSubmit={e => {
+        e.preventDefault();
+        addTweet({variables: {
+          title: title.value,
+          user: user.value,
+          body: body.value
+        }});
+        input.value = '';
+      }}
+    >
       <Grid container spacing={2}>
         <Grid item xs={12} md="3">
-          <input required label="Title" placeholder="Title" className="pinterest-form__field" />
+          <input
+            required
+            ref={node => {
+              title = node;
+            }}
+            placeholder="Title"
+            className="pinterest-form__field" />
         </Grid>
         <Grid item xs={12} md="3">
-          <input required label="Author"  placeholder="Author" className="pinterest-form__field" />
+          <input
+            required
+            ref={node => {
+              user = node;
+            }}
+            placeholder="Author"
+            className="pinterest-form__field" />
         </Grid>
 
         <Grid item xs={12} md="3">
-          <input required label="Body"  placeholder="Body" className="pinterest-form__field" />
+          <input
+            required
+            ref={node => {
+              body = node;
+            }}
+            placeholder="Body"
+            className="pinterest-form__field"
+          />
         </Grid>
         <Grid item xs={12} md="3">
           <Button size="small" className="pinterest-form__button">Save</Button>
         </Grid>
       </Grid>
-    </div>
+    </form>
   )
 }
